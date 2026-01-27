5cedcbb5013c1e3046992e00f658cc1a2563af8d
erizhm <ecpv.glory@gmail.com>
2026-01-27 23:45:59 +0700

    Chore: read projects list, project detail, generate ai template, and FIX: router, models

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/userController');
 const authenticate = require('../middlewares/authentication.js')
+const ProjectRouter = require('./projectRouter.js');
+
+router.use('/projects', authenticate, ProjectRouter)
 
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
