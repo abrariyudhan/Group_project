2f4480c50a55cb5e74c52e7b91ed895115b6ab95
yosua <yosuamarbun17@gmail.com>
2026-01-27 23:56:52 +0700

    fix: add authorization

---
+++ server/middlewares/authorization.js
@@ -0,0 +1,12 @@
+module.exports = function authorization(req, res, next) {
+    try {
+        // cek apakah req.user ada (harusnya sudah di-set oleh middleware authentication)
+        if (!req.user) {
+            // jika req.user tidak ada, lempar error Unauthorized
+            throw { name: 'Unauthorized', message: 'User not authenticated' };
+        }
+    } catch (error) { // lempar error ke errorHandler middleware
+        return next(error);
+        
+    }
+}
