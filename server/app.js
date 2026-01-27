ebb14497f63b5a1fc05e257d012dd1891e86f0b4
erizhm <ecpv.glory@gmail.com>
2026-01-27 22:44:07 +0700

    Chore: Add register and login features

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 const router = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
 
@@ -9,3 +12,7 @@ app.use(express.json())
 
 app.use(router)
 app.use(errorHandler)
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,4 +3,7 @@ const router = express.Router();
 const UserController = require('../controllers/userController');
 const authenticate = require('../middlewares/authentication.js')
 
+router.post('/register', UserController.register)
+router.post('/login', UserController.login)
+
 module.exports = router
