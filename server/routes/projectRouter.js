589c72770b3e5b16204d1daa203c0302fabe7fb8
yosua <yosuamarbun17@gmail.com>
2026-01-28 17:51:36 +0700

    test: no change

--- server/routes/projectRouter.js
+++ server/routes/projectRouter.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const ProjectController = require("../controllers/projectController")
 
 router.get('/', ProjectController.getProjects)
-router.post('/', ProjectController.createProject)
+router.get('/generate-ai', ProjectController.generateAiProject)
 router.post('/generate-ai', ProjectController.generateAiProject)
 router.get('/:projectId', ProjectController.getProjectDetail)
 
